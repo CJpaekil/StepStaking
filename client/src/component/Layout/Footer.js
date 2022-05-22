4a9ce42ff58fad048925669ef73346f61936e59d
Senior_DEV <86672166+CJpaekil@users.noreply.github.com>
2022-05-22 09:38:26 +0800

    all updated

--- client/src/component/Layout/Footer.js
+++ client/src/component/Layout/Footer.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 const Footer = () => {
     const [pageExchange, setPageExchange] = useState("main");
     return (
-        <div className={`${pageExchange === "main" ? " main-navbar" : "claim-navbar"}`}>
+        <div className={`${pageExchange === "main" ? " main-footbar" : "claim-navbar"}`}>
             <div className="container d-flex">
                 <div className="col-sm-6 col-6 d-flex justify-content-start align-items-center">
                     <img className="logo" src="/assets/images/Vector.png" />
